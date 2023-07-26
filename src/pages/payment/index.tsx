e1f435d5b51394f35f7dadc275d85021073cee2f
michiru6828 <since_l999@yahoo.co.jp>
2023-07-26 14:56:02 -0700

    set card info input

--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Payment() {
-  return <div></div>
+  const [cardInfo, setCardInfo] = useState({
+    cardNumber: '',
+    expireMonth: '',
+    expireYear: '',
+    cardHolder: '',
+  })
+
+  console.log(cardInfo)
+  const handleOnchange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+    field: string
+  ) => {
+    //オブジェクトのkey名を変数で指定するなら[]が必要
+    setCardInfo((prev) => ({ ...prev, [field]: e.target.value }))
+  }
+  const currentYear = new Date().getFullYear()
+  //Array.fromで新しい配列を生成。長さは10,第二引数は生成する各要素に対して実行する関数
+  const years = Array.from({ length: 10 }, (_, index) => currentYear + index)
+  const months = Array.from({ length: 12 }, (_, i) => i + 1)
+  return (
+    <form>
+      <label htmlFor='cardNumber'>
+        カード番号
+        <input onChange={(e) => handleOnchange(e, 'cardNumber')} type='text' />
+      </label>
+
+      <div>
+        <label htmlFor='expireDate'>
+          有効期限
+          <select
+            onChange={(e) => handleOnchange(e, 'expireMonth')}
+            name='expireDate'
+            id='expireDate'
+          >
+            {months.map((month) => (
+              <option value={month} key={month}>
+                {month}月
+              </option>
+            ))}
+          </select>
+          ／
+          <select
+            onChange={(e) => handleOnchange(e, 'expireYear')}
+            name='expireYear'
+            id='expireYear'
+          >
+            {years.map((year) => (
+              <option value={year} key={year}>
+                {year}年
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+
+      <label htmlFor='cardHolder'>
+        カード名義人
+        <input onChange={(e) => handleOnchange(e, 'cardHolder')} type='text' />
+      </label>
+      <input type='submit' value='購入' />
+    </form>
+  )
 }
 
 export default Payment
