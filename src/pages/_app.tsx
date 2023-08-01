2e69784c7b9962380e25fc65bff0cd542defd5c7
michiru6828 <since_l999@yahoo.co.jp>
2023-07-31 17:13:13 -0700

    header icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <link rel='icon' sizes='16x16' href='/dogIcon.png' />
+      </Head>
       <Component {...pageProps} />
     </Provider>
   )
